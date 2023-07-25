f424320d926a4d77876559d53a5192b72f22aeb9
João Vieira <netojoao599@gmail.com>
2023-07-25 14:50:37 -0300

    verificationImc

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,5 +1,10 @@
 import React, {useState} from "react";
-import { TextInput, View, Text, TouchableOpacity } from "react-native";
+import { 
+    TextInput, 
+    View, 
+    Text, 
+    TouchableOpacity,
+ } from "react-native";
 import ResultImc from "./ResultImc";
 import styles from "./style";
 
@@ -11,9 +16,16 @@ const [weight, setWeight] = useState(null)
 const [messageImc, setMessageImc] = useState("preencha o peso e altura");
 const [imc, setImc] = useState(null)
 const [textButton, setTextButton] = useState("Calcular")
+const [errorMessage, setErrorMessage] = useState(null);
 
 function imcCalculator() {
     return setImc((weight/(height*height)).toFixed(2))
+}
+
+function verificationImc() {
+    if (imc == null) {
+        setErrorMessage("campo obrigatório*")
+    }
 }
 
 function validationImc() {
@@ -23,17 +35,21 @@ function validationImc() {
         setWeight(null)
         setMessageImc("Seu imc é igual:")
         setTextButton("Calcular Novamente")
+        setErrorMessage(null)
         return
     }
+    verificationImc()
     setImc(null)
     setTextButton("Calcular")
     setMessageImc("preencha o peso e altura")
+    
 }
 
     return(
         <View style={styles.formContext}>
             <View style={styles.form}>
                 <Text style={styles.formLabel}>Altura</Text> 
+                <Text style={styles.errorMessage}>{errorMessage}</Text>
                 <TextInput style={styles.input}
                     onChangeText={setHeight}
                     value={height}
@@ -42,6 +58,7 @@ function validationImc() {
                 />
 
                 <Text style={styles.formLabel}>Peso</Text>
+                <Text style={styles.errorMessage}>{errorMessage}</Text>
                 <TextInput style={styles.input}
                     onChangeText={setWeight}
                     value={weight}
