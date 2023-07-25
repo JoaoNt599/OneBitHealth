731bc47da02a070307c55bcadc164e564adbec94
João Vieira <netojoao599@gmail.com>
2023-07-25 16:55:34 -0300

    Pressable and Keyboard api

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -4,7 +4,9 @@ import {
     View, 
     Text, 
     TouchableOpacity,
-    Vibration
+    Vibration,
+    Pressable,
+    Keyboard,
  } from "react-native";
 import ResultImc from "./ResultImc";
 import styles from "./style";
@@ -20,7 +22,8 @@ const [textButton, setTextButton] = useState("Calcular")
 const [errorMessage, setErrorMessage] = useState(null);
 
 function imcCalculator() {
-    return setImc((weight/(height*height)).toFixed(2))
+    let heightFormat = height.replace(",",".")
+    return setImc((weight/(heightFormat*heightFormat)).toFixed(2))
 }
 
 function verificationImc() {
@@ -48,7 +51,7 @@ function validationImc() {
 }
 
     return(
-        <View style={styles.formContext}>
+        <Pressable onPress={Keyboard.dismiss} style={styles.formContext}>
             <View style={styles.form}>
                 <Text style={styles.formLabel}>Altura</Text> 
                 <Text style={styles.errorMessage}>{errorMessage}</Text>
@@ -78,6 +81,6 @@ function validationImc() {
                 </TouchableOpacity>
             </View>
             <ResultImc messageResultImc={messageImc} resultImc={imc}/>
-        </View>
+        </Pressable>
     );
 }
